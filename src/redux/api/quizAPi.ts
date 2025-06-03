5e63e91b9207e6cae0374a349c1a489a2842bf97
MD JABED HASAN <138822808+Jabed-Hasan@users.noreply.github.com>
2025-06-03 09:01:49 +0600

    Change BaseURL

--- src/redux/api/quizAPi.ts
+++ src/redux/api/quizAPi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const quizApi = createApi({
   reducerPath: 'quizApi',
   baseQuery: fetchBaseQuery({ 
-    baseUrl: 'http://localhost:5000/api'
+    baseUrl: 'https://quiz-app-server-ruddy.vercel.app/api'
   }),
   tagTypes: ['Quiz'],
   endpoints: (builder) => ({
